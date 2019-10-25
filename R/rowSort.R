f2618c9493fc134ee1d988e0c980be831d4cb084
Hervé Pagès <hpages@fredhutch.org>
2019-10-25 01:24:13 -0700

    document rowNthLargest()

--- R/rowSort.R
+++ R/rowSort.R
@@ -1,3 +1,22 @@
+### =========================================================================
+### rowSort() and related functions
+### -------------------------------------------------------------------------
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### rowSort()
+###
+
+## A simple but inefficient implementation of rowSort().
+simple_rowSort <- function(x, decreasing=FALSE)
+{
+    sorted_rows <- lapply(seq_len(nrow(x)),
+                          function(i) sort(x[i, ], decreasing=decreasing))
+    ans <- matrix(unlist(sorted_rows), nrow=nrow(x), byrow=TRUE)
+    rownames(ans) <- rownames(x)
+    ans
+}
+
 ### Setting 'decreasing' to TRUE has the effect of flipping the returned
 ### matrix with respect to the vertical axis. More precisely: if x1 is
 ### rowSort(x) then rowSort(x, decreasing=TRUE) should be identical to
@@ -14,17 +33,46 @@ rowSort <- function(x, decreasing=FALSE, use.radix.sort=NA)
                        PACKAGE="sparseMatrixColVars")
 }
 
-### A fast pure base R implementation of rowSort(x)[ , ncol(x) - nth + 1L]
-baseR_rowNthLargest <- function(x, nth=1L)
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### rowNthLargest()
+###
+### rowNthLargest(x, nth) returns the n-th largest value in each row i.e.
+### rowSort(x, decreasing=TRUE)[ , nth]
+###
+
+.normarg_nth <- function(nth, x_ncol)
 {
-    stopifnot(is.matrix(x),
-              is.numeric(nth), length(nth) == 1L, !is.na(nth))
+    if (!isSingleNumber(nth))
+        stop("'nth' must be a single integer")
     if (!is.integer(nth))
         nth <- as.integer(nth)
-    x_nrow <- nrow(x)
-    x_ncol <- ncol(x)
     if (nth < 1L || nth > x_ncol)
         stop("'nth' must be >= 1 and <= ncol(x)")
+    nth
+}
+
+### A simple but inefficient implementation of rowNthLargest().
+simple_rowNthLargest <- function(x, nth=1L)
+{
+    stopifnot(is.matrix(x))
+    x_ncol <- ncol(x)
+    nth <- .normarg_nth(nth, x_ncol)
+
+    apply(x, 1,
+          function(row) {
+              n <- x_ncol - nth + 1L
+              sort(row, partial=n)[n]
+          })
+}
+
+### A fast pure base R implementation of rowNthLargest().
+baseR_rowNthLargest <- function(x, nth=1L)
+{
+    stopifnot(is.matrix(x))
+    x_nrow <- nrow(x)
+    x_ncol <- ncol(x)
+    nth <- .normarg_nth(nth, x_ncol)
 
     row_seq <- seq_len(x_nrow)
     ## Order first by row, then by value within the row.
@@ -32,10 +80,11 @@ baseR_rowNthLargest <- function(x, nth=1L)
     ## but slightly faster.
     oo <- order(rep.int(row_seq, x_ncol), x)
     idx <- oo[row_seq * x_ncol - nth + 1L]
-    x[idx]
+    setNames(x[idx], rownames(x))
 }
 
-### An optimized implementation of rowSort(x)[ , ncol(x) - nth + 1L]
+### Optimized implementation.
+### Supports an 'nth' of length the number of rows in 'x'.
 rowNthLargest <- function(x, nth=1L)
 {
     stopifnot(is.matrix(x),
